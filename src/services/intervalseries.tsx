612e500048b78583d3fe58d400cbb6f1547390ec
nerfthisdev <353054@niuitmo.ru>
2025-04-24 22:52:22 +0300

    feat: complete interval series class and its methods

--- src/services/intervalseries.tsx
+++ src/services/intervalseries.tsx
@@ -98,4 +98,100 @@ class IntervalVariationSeries {
         }
         return this._binCenters
     }
+
+    get expectedValue(): number {
+        if (this._expectedValue === null) {
+            let total = 0
+            const centers = this.binCenters
+            const counts = Object.values(this.statisticalSeries)
+            for (let i = 0; i < centers.length; i++) {
+                total += centers[i] * counts[i]
+            }
+            this._expectedValue = total / this.n
+        }
+        return this._expectedValue
+    }
+
+    get sampleVariance(): number {
+        if (this._sampleVariance === null) {
+            const mean = this.expectedValue
+            let total = 0
+            const centers = this.binCenters
+            const counts = Object.values(this.statisticalSeries)
+            for (let i = 0; i < centers.length; i++) {
+                total += (centers[i] - mean) ** 2 * counts[i]
+            }
+            this._sampleVariance = total / this.n
+        }
+        return this._sampleVariance
+    }
+
+    get sampleStandardDeviation(): number {
+        return Math.sqrt(this.sampleVariance)
+    }
+
+    get mode(): number {
+        const counts = Object.values(this.statisticalSeries)
+        const maxCount = Math.max(...counts)
+        const modalIdx = counts.indexOf(maxCount)
+        const borders = this.intervalBorders
+
+        const prevCount = modalIdx > 0 ? counts[modalIdx - 1] : 0
+        const nextCount = modalIdx < counts.length - 1 ? counts[modalIdx + 1] : 0
+
+        return (
+            borders[modalIdx] +
+            this.intervalLength *
+            ((maxCount - prevCount) /
+                (maxCount - prevCount + (maxCount - nextCount)))
+        )
+    }
+
+    get median(): number {
+        const counts = Object.values(this.statisticalSeries)
+        const cumulative: Array<number> = []
+        let total = 0
+        for (const count of counts) {
+            total += count
+            cumulative.push(total)
+        }
+
+        const target = this.n / 2
+        let medianIdx = 0
+        while (medianIdx < cumulative.length && cumulative[medianIdx] < target) {
+            medianIdx++
+        }
+
+        const lowerBound = this.intervalBorders[medianIdx]
+        if (medianIdx === 0) {
+            return lowerBound + (target / counts[medianIdx]) * this.intervalLength
+        } else {
+            const prevTotal = cumulative[medianIdx - 1]
+            return (
+                lowerBound +
+                ((target - prevTotal) / counts[medianIdx]) * this.intervalLength
+            )
+        }
+    }
+
+    getNthMoment(moment: number): number {
+        const mean = this.expectedValue
+        const stdDev = this.sampleStandardDeviation
+        let total = 0
+        const centers = this.binCenters
+        const counts = Object.values(this.statisticalSeries)
+
+        for (let i = 0; i < centers.length; i++) {
+            total += (centers[i] - mean) ** moment * counts[i]
+        }
+        return total / (this.n * stdDev ** moment)
+    }
+
+    getCdf(x: number): number {
+        let count = 0
+        for (const value of this.data) {
+            if (value < x) count++
+        }
+        return count / this.n
+    }
 }
