31accc52f62b7d8479e707586c4c59cf89ee8c34
nerfthisdev <353054@niuitmo.ru>
2025-04-28 09:09:34 +0300

    feat: add complete variation series

--- src/services/variationSeries.ts
+++ src/services/variationSeries.ts
@@ -1,26 +1,22 @@
-interface StatisticalSeries {
-  [key: number]: number;
-}
+import { cdf } from 'statistics.js';
 
 export class Variation {
   private data: Array<number>;
   private _n: number;
   private _min: number;
   private _max: number;
-  private _statisticalSeries: StatisticalSeries;
-  private _mode: number;
+  private _statisticalSeries: Record<string, number> = {};
+  private _mode: number | null = null;
+  private _median: number | null = null;
+  private _expectedValueEstimate: number | null = null;
+  private _sampleVariance: number | null = null;
+  private _cumulativeValues: Array<number> = [];
 
   constructor(data: Array<number>) {
     this.data = [...data].sort((a, b) => a - b);
     this._n = data.length;
     this._min = Math.min(...data);
     this._max = Math.max(...data);
-    this._statisticalSeries = this.calcStatisticalSeries();
-    this._mode = this.getMode();
-  }
-
-  get variationseries(): Array<number> {
-    return this.data;
   }
 
   get n(): number {
@@ -34,29 +30,125 @@ export class Variation {
   get max(): number {
     return this._max;
   }
-  get statisticalSeries(): StatisticalSeries {
+
+  get range(): number {
+    return this.max - this.min;
+  }
+
+  get statisticalSeries(): Record<string, number> {
+    if (Object.keys(this._statisticalSeries).length === 0) {
+      const freqMap: Record<string, number> = {};
+      for (const num of this.data) {
+        const key = num.toString();
+        freqMap[key] = (freqMap[key] || 0) + 1;
+      }
+      this._statisticalSeries = freqMap;
+    }
     return this._statisticalSeries;
   }
 
-  private calcStatisticalSeries(): StatisticalSeries {
-    return this.data.reduce((acc: StatisticalSeries, num) => {
-      acc[num] = (acc[num] || 0) + 1;
-      return acc;
-    }, {});
+  get mode(): number {
+    if (!this._mode) {
+      let maxCount = -Infinity;
+      let modeValue = NaN;
+
+      for (const [key, count] of Object.entries(this.statisticalSeries)) {
+        if (count > maxCount) {
+          maxCount = count;
+          modeValue = parseFloat(key);
+        }
+      }
+      this._mode = modeValue;
+    }
+    return this._mode;
   }
 
-  private getMode(): number {
-    let maxCount = 0;
-    let modeValue = this.data[0];
-
-    Object.entries(this.statisticalSeries).forEach(([key, count]) => {
-      const numCount = Number(count);
-      if (numCount > maxCount) {
-        maxCount = numCount;
-        modeValue = Number(key);
+  get median(): number {
+    if (!this._median) {
+      const n = this.data.length;
+      if (n % 2 === 0) {
+        const mid = n / 2;
+        this._median = (this.data[mid - 1] + this.data[mid]) / 2;
+      } else {
+        this._median = this.data[Math.floor(n / 2)];
       }
-    });
+    }
+    return this._median;
+  }
 
-    return modeValue;
+  get expectedValueEstimate(): number {
+    if (!this._expectedValueEstimate) {
+      this._expectedValueEstimate =
+        this.data.reduce((sum, val) => sum + val, 0) / this.data.length;
+    }
+    return this._expectedValueEstimate;
+  }
+
+  get expectedValueDeviation(): number {
+    const mean = this.expectedValueEstimate;
+    return this.data.reduce((sum, val) => sum + (val - mean), 0);
+  }
+
+  get sampleVariance(): number {
+    if (!this._sampleVariance) {
+      const mean = this.expectedValueEstimate;
+      this._sampleVariance =
+        this.data.reduce((sum, val) => sum + Math.pow(val - mean, 2), 0) /
+        this.data.length;
+    }
+    return this._sampleVariance;
+  }
+
+  get sampleStandardDeviation(): number {
+    return Math.sqrt(this.sampleVariance);
+  }
+
+  get sampleStandardDeviationCorrected(): number {
+    return Math.sqrt(
+      this.sampleVariance * (this.data.length / (this.data.length - 1)),
+    );
+  }
+
+  get cumulativeValues(): Array<number> {
+    if (this._cumulativeValues.length === 0) {
+      let prev = 0;
+      this._cumulativeValues = Object.values(this.statisticalSeries).map(
+        (count) => {
+          const current = prev;
+          prev += count;
+          return current;
+        },
+      );
+    }
+    return this._cumulativeValues;
+  }
+
+  getNthMoment(nthOrder: number): number {
+    const mean = this.expectedValueEstimate;
+    return (
+      this.data.reduce((sum, val) => sum + Math.pow(val - mean, nthOrder), 0) /
+      this.data.length
+    );
+  }
+
+  getCdf(x: number): number {
+    let count = 0;
+    for (const value of this.data) {
+      if (value < x) count++;
+    }
+    return count / this.data.length;
   }
 }
+
+// find a library with these functions (or make them yourself)
+const laplaceFunction = (x: number): number => {
+  return 0;
+};
+
+const studentCoefficient = (gamma: number, n: number): number => {
+  return 0;
+};
+
+const getInverseLaplace = (alpha: number): number => {
+  return 0;
+};
